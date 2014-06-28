4c8cd815f9fb14706e4bb0f7fff3d1e706abbf37
Martin Schulze <schulmar@hrz.tu-chemnitz.de>
2014-06-28 14:36:07 +0200

    Avoid serialization warning

--- src/net/github/clang_formateclipse/ClangFormatError.java
+++ src/net/github/clang_formateclipse/ClangFormatError.java
@@ -2,4 +2,9 @@ package net.github.clang_formateclipse;
 
 public class ClangFormatError extends Exception {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 }
--- src/net/github/clang_formateclipse/UnsupportedClangVersion.java
+++ src/net/github/clang_formateclipse/UnsupportedClangVersion.java
@@ -2,6 +2,10 @@ package net.github.clang_formateclipse;
 
 public class UnsupportedClangVersion extends Exception {
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private ClangVersion version;
 	
 	public UnsupportedClangVersion(ClangVersion version) {
